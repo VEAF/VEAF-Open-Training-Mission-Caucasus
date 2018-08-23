ce114aa96cf61763eb2923da7cb739197536f20e
David Pierron <dpierron@gmail.com>
2018-08-23 14:34:36 +0200

    cleaned up script

--- scripts/VEAF_a2g_mission_generator.lua
+++ scripts/VEAF_a2g_mission_generator.lua
@@ -78,7 +78,7 @@ veafCas.RedSpawnedUnitsGroupName = "VEAF Spawned Units"
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --- Version.
-veafCas.version = "0.1.1"
+veafCas.version = "0.1.2"
 
 --- Identifier. All output in DCS.log will start with this.
 veafCas.id = "VEAF "
@@ -148,41 +148,6 @@ function veafCas.getAveragePosition(groupName)
 end
 
 function veafCas.emptyFunction()
-end
-
---- Find a suitable point for spawning a unit in a <dispersion>-sized circle around a spot
-function veafCas.findPointInZone(spawnSpot, dispersion)
-    local unitPosition
-    local tryCounter = 1000
-    repeat -- Place the unit in a "dispersion" ft radius circle from the spawn spot
-        unitPosition = mist.getRandPointInCircle(spawnSpot, dispersion)
-        tryCounter = tryCounter - 1
-    until (land.getSurfaceType(unitPosition) == land.SurfaceType.LAND or land.getSurfaceType(unitPosition) == land.SurfaceType.ROAD or land.getSurfaceType(unitPosition) == land.SurfaceType.RUNWAY) or tryCounter == 0
-    if tryCounter == 0 then
-        return nil
-    else
-        return unitPosition
-    end
-end
-
---- Add a unit to the <group> on a suitable point in a <dispersion>-sized circle around a spot
-function veafCas.addUnit(group, spawnSpot, dispersion, unitType, unitName, skill)
-    local unitPosition = veafCas.findPointInZone(spawnSpot, dispersion)
-    if unitPosition ~= nil then
-        table.insert(
-            group,
-            {
-                ["x"] = unitPosition.x,
-                ["y"] = unitPosition.y,
-                ["type"] = unitType,
-                ["name"] = unitName,
-                ["heading"] = 0,
-                ["skill"] = skill
-            }
-        )
-    else
-        veafCas.logInfo("cannot find a suitable position for unit "..unitType)
-    end
 end
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -386,8 +351,43 @@ end
 -- CAS target group generation and management
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+--- Find a suitable point for spawning a unit in a <dispersion>-sized circle around a spot
+function veafCas.findPointInZone(spawnSpot, dispersion)
+    local unitPosition
+    local tryCounter = 1000
+    repeat -- Place the unit in a "dispersion" ft radius circle from the spawn spot
+        unitPosition = mist.getRandPointInCircle(spawnSpot, dispersion)
+        tryCounter = tryCounter - 1
+    until (land.getSurfaceType(unitPosition) == land.SurfaceType.LAND or land.getSurfaceType(unitPosition) == land.SurfaceType.ROAD or land.getSurfaceType(unitPosition) == land.SurfaceType.RUNWAY) or tryCounter == 0
+    if tryCounter == 0 then
+        return nil
+    else
+        return unitPosition
+    end
+end
+
+--- Add a unit to the <group> on a suitable point in a <dispersion>-sized circle around a spot
+function veafCas.addUnit(group, spawnSpot, dispersion, unitType, unitName, skill)
+    local unitPosition = veafCas.findPointInZone(spawnSpot, dispersion)
+    if unitPosition ~= nil then
+        table.insert(
+            group,
+            {
+                ["x"] = unitPosition.x,
+                ["y"] = unitPosition.y,
+                ["type"] = unitType,
+                ["name"] = unitName,
+                ["heading"] = 0,
+                ["skill"] = skill
+            }
+        )
+    else
+        veafCas.logInfo("cannot find a suitable position for unit "..unitType)
+    end
+end
+
+--- Generates an air defense group
 function veafCas.generateAirDefenseGroup(groupId, spawnSpot, defense, armor, skill)
-    -- generate an air defense group
     local infantryGroup = {}
     local vehiclesGroup = {}
 
@@ -434,8 +434,8 @@ function veafCas.generateAirDefenseGroup(groupId, spawnSpot, defense, armor, ski
     return vehiclesGroup, infantryGroup
 end
 
+--- Generates a transport company and its air defenses
 function veafCas.generateTransportCompany(groupId, spawnSpot, defense, armor, skill)
-    -- generate a transport company and its air defenses
     local infantryGroup = {}
     local vehiclesGroup = {}
 
@@ -471,17 +471,17 @@ function veafCas.generateTransportCompany(groupId, spawnSpot, defense, armor, sk
     -- add an air defense vehicle
     if defense > 2 then 
         -- defense = 3-5 : add a Shilka
-        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, unitTypes.vehicles.SAM.ZSU234_Shilka, veafCas.RedCasVehiclesGroupName .. " Transport Company #" .. groupId .. " Air Defense Unit", skill)
+        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, "ZSU-23-4 Shilka", veafCas.RedCasVehiclesGroupName .. " Transport Company #" .. groupId .. " Air Defense Unit", skill)
     elseif defense > 0 then
         -- defense = 1 : add a ZU23 on a truck
-        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, unitTypes.vehicles.SAM.Ural375_ZU23, veafCas.RedCasVehiclesGroupName .. " Transport Company #" .. groupId .. " Air Defense Unit", skill)
+        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, "Ural-375 ZU-23", veafCas.RedCasVehiclesGroupName .. " Transport Company #" .. groupId .. " Air Defense Unit", skill)
     end
 
     return vehiclesGroup, infantryGroup
 end
 
+--- Generates an armor platoon and its air defenses
 function veafCas.generateArmorPlatoon(groupId, spawnSpot, defense, armor, skill)
-    -- generate an armor platoon and its air defenses
     local infantryGroup = {}
     local vehiclesGroup = {}
 
@@ -538,16 +538,16 @@ function veafCas.generateArmorPlatoon(groupId, spawnSpot, defense, armor, skill)
     -- add an air defense vehicle
     if defense > 3 then 
         -- defense = 4-5 : add a Tunguska
-        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, unitTypes.vehicles.SAM._2S6_Tunguska, veafCas.RedCasVehiclesGroupName .. " Armor Platoon #" .. groupId .. " Air Defense Unit", skill)
+        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, "2S6 Tunguska", veafCas.RedCasVehiclesGroupName .. " Armor Platoon #" .. groupId .. " Air Defense Unit", skill)
     elseif defense > 0 then
         -- defense = 1-3 : add a Shilka
-        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, unitTypes.vehicles.SAM.ZSU234_Shilka, veafCas.RedCasVehiclesGroupName .. " Armor Platoon #" .. groupId .. " Air Defense Unit", skill)
+        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, "ZSU-23-4 Shilka", veafCas.RedCasVehiclesGroupName .. " Armor Platoon #" .. groupId .. " Air Defense Unit", skill)
     end
 
     return vehiclesGroup, infantryGroup
 end
 
--- generate an infantry group along with its manpad units and tranport vehicles
+--- Generates an infantry group along with its manpad units and tranport vehicles
 function veafCas.generateInfantryGroup(groupId, spawnSpot, defense, armor, skill)
     local infantryGroup = {}
     local vehiclesGroup = {}
@@ -556,22 +556,24 @@ function veafCas.generateInfantryGroup(groupId, spawnSpot, defense, armor, skill
     local groupCount = mist.random(3, 7)
     local dispersion = (groupCount+1) * 10 + 25
     for i = 1, groupCount do
-        veafCas.addUnit(infantryGroup, spawnSpot, dispersion, unitTypes.vehicles.IFV.Soldier_AK, veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " unit #" .. i, skill)
+        veafCas.addUnit(infantryGroup, spawnSpot, dispersion, "Soldier AK", veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " unit #" .. i, skill)
     end
 
     -- add a transport vehicle
     if armor > 0 then
-        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, unitTypes.vehicles.IFV.BTR80, veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " APC", skill)
+        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, "BTR-80", veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " APC", skill)
+    else
+        veafCas.addUnit(vehiclesGroup, spawnSpot, dispersion, "GAZ-3307", veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " truck", skill) -- TODO check if tranport type is correct
     end
 
     -- add manpads if needed
     if defense > 3 then
         -- for defense = 4-5, spawn a full Igla-S team
-        veafCas.addUnit(infantryGroup, spawnSpot, dispersion, unitTypes.vehicles.SAM.SA18_IglaS_comm, veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " manpad COMM soldier", skill)
-        veafCas.addUnit(infantryGroup, spawnSpot, dispersion, unitTypes.vehicles.SAM.SA18_IglaS_manpad, veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " manpad launcher soldier", skill)
+        veafCas.addUnit(infantryGroup, spawnSpot, dispersion, "SA-18 Igla-S comm", veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " manpad COMM soldier", skill)
+        veafCas.addUnit(infantryGroup, spawnSpot, dispersion, "SA-18 Igla-S manpad", veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " manpad launcher soldier", skill)
     elseif defense > 0 then
         -- for defense = 1-3, spawn a single Igla soldier
-        veafCas.addUnit(infantryGroup, spawnSpot, dispersion, unitTypes.vehicles.SAM.SA18_Igla_manpad, veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " manpad launcher soldier", skill)
+        veafCas.addUnit(infantryGroup, spawnSpot, dispersion, "SA-18 Igla manpad", veafCas.RedCasInfantryGroupName .. " Infantry Platoon #" .. groupId .. " manpad launcher soldier", skill)
     else
         -- for defense = 0, don't spawn any manpad
     end
@@ -579,6 +581,7 @@ function veafCas.generateInfantryGroup(groupId, spawnSpot, defense, armor, skill
     return vehiclesGroup, infantryGroup
 end
 
+--- Generates a complete CAS target group
 function veafCas.generateCasMission(spawnSpot, size, defense, armor, spacing, skill, disperseOnAttack)
     local infantryUnits = {}
     local vehiclesUnits = {}
@@ -724,6 +727,7 @@ function veafCas.generateCasMission(spawnSpot, size, defense, armor, spacing, sk
     veafCas.casGroupWatchdog()
 end
 
+--- Checks if the vehicles group is still alive, and if not announces the end of the CAS mission
 function veafCas.casGroupWatchdog() 
     local group = Group.getByName(veafCas.RedCasVehiclesGroupName)
     if group and group:isExist() == true and #group:getUnits() > 0 then
@@ -734,12 +738,14 @@ function veafCas.casGroupWatchdog()
     end
 end
 
+--- Called from the "Skip target" radio menu : remove the current CAS target group
 function veafCas.skipCasTarget()
     trigger.action.outText("Skipping CAS objective group ; please stand by...", 5)
     veafCas.cleanupAfterMission()
     trigger.action.outText("CAS objective group cleaned up.", 5)
 end
 
+--- Cleanup after either mission is ended or aborted
 function veafCas.cleanupAfterMission()
     -- destroy vehicles and infantry groups
     local group = Group.getByName(veafCas.RedCasVehiclesGroupName)
@@ -764,6 +770,7 @@ function veafCas.cleanupAfterMission()
     -- TODO add the create task info menu
 end
 
+--- Build the initial radio menu
 function veafCas.buildRadioMenu()
     veafCas._taskingsRootPath = missionCommands.addSubMenu('Tasking')
     veafCas._taskingsGroundPath = missionCommands.addSubMenu('Ground', veafCas._taskingsRootPath)
@@ -774,6 +781,8 @@ end
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Unit spawn command
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--- Spawn a specific unit at a specific spot
 function veafCas.spawnUnit(spawnSpot, unitType)
     veafCas.logDebug("generateUnit(spawnSpot = " .. spawnSpot .. ", unitType = " .. unitType .. ")")
   
@@ -821,199 +830,3 @@ veafCas.buildRadioMenu()
 
 --- Add event handler.
 world.addEventHandler(veafCas.eventHandler)
-
--------------------------------------------------------------------------------------------------------------------------------------------------------------
--- Unit types table
--------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-unitTypes = {}
-unitTypes.navy = {}
-unitTypes.navy.blue = {
-  VINSON = "VINSON",
-  PERRY = "PERRY",
-  TICONDEROG = "TICONDEROG"
-}
-unitTypes.navy.red = {
-  ALBATROS = "ALBATROS",
-  KUZNECOW = "KUZNECOW",
-  MOLNIYA = "MOLNIYA",
-  MOSCOW = "MOSCOW",
-  NEUSTRASH = "NEUSTRASH",
-  PIOTR = "PIOTR",
-  REZKY = "REZKY"
-}
-unitTypes.navy.civil = {
-  ELNYA = "ELNYA",
-  Drycargo_ship2 = "Dry-cargo ship-2",
-  Drycargo_ship1 = "Dry-cargo ship-1",
-  ZWEZDNY = "ZWEZDNY"
-}
-unitTypes.navy.submarine = {
-  KILO = "KILO",
-  SOM = "SOM"
-}
-unitTypes.navy.speedboat = {
-  speedboat = "speedboat"
-}
-unitTypes.vehicles = {}
-unitTypes.vehicles.Howitzers = {
-  _2B11_mortar = "2B11 mortar",
-  SAU_Gvozdika = "SAU Gvozdika",
-  SAU_Msta = "SAU Msta",
-  SAU_Akatsia = "SAU Akatsia",
-  SAU_2C9 = "SAU 2-C9",
-  M109 = "M-109"
-}
-unitTypes.vehicles.IFV = {
-  AAV7 = "AAV7",
-  BMD1 = "BMD-1",
-  BMP1 = "BMP-1",
-  BMP2 = "BMP-2",
-  BMP3 = "BMP-3",
-  Boman = "Boman",
-  BRDM2 = "BRDM-2",
-  BTR80 = "BTR-80",
-  BTR_D = "BTR_D",
-  Bunker = "Bunker",
-  Cobra = "Cobra",
-  LAV25 = "LAV-25",
-  M1043_HMMWV_Armament = "M1043 HMMWV Armament",
-  M1045_HMMWV_TOW = "M1045 HMMWV TOW",
-  M1126_Stryker_ICV = "M1126 Stryker ICV",
-  M113 = "M-113",
-  M1134_Stryker_ATGM = "M1134 Stryker ATGM",
-  M2_Bradley = "M-2 Bradley",
-  Marder = "Marder",
-  MCV80 = "MCV-80",
-  MTLB = "MTLB",
-  Paratrooper_RPG16 = "Paratrooper RPG-16",
-  Paratrooper_AKS74 = "Paratrooper AKS-74",
-  Sandbox = "Sandbox",
-  Soldier_AK = "Soldier AK",
-  Infantry_AK = "Infantry AK",
-  Soldier_M249 = "Soldier M249",
-  Soldier_M4 = "Soldier M4",
-  Soldier_M4_GRG = "Soldier M4 GRG",
-  Soldier_RPG = "Soldier RPG",
-  TPZ = "TPZ"
-}
-unitTypes.vehicles.MLRS = {
-  GradURAL = "Grad-URAL",
-  Uragan_BM27 = "Uragan_BM-27",
-  Smerch = "Smerch",
-  MLRS = "MLRS"
-}
-unitTypes.vehicles.SAM = {
-  _2S6_Tunguska = "2S6 Tunguska",
-  Kub_2P25_ln = "Kub 2P25 ln",
-  _5p73_s125_ln = "5p73 s-125 ln",
-  S300PS_5P85C_ln = "S-300PS 5P85C ln",
-  S300PS_5P85D_ln = "S-300PS 5P85D ln",
-  SA11_Buk_LN_9A310M1 = "SA-11 Buk LN 9A310M1",
-  Osa_9A33_ln = "Osa 9A33 ln",
-  Tor_9A331 = "Tor 9A331",
-  Strela10M3 = "Strela-10M3",
-  Strela1_9P31 = "Strela-1 9P31",
-  SA11_Buk_CC_9S470M1 = "SA-11 Buk CC 9S470M1",
-  SA8_Osa_LD_9T217 = "SA-8 Osa LD 9T217",
-  Patriot_AMG = "Patriot AMG",
-  Patriot_ECS = "Patriot ECS",
-  Gepard = "Gepard",
-  Hawk_pcp = "Hawk pcp",
-  SA18_Igla_manpad = "SA-18 Igla manpad",
-  SA18_Igla_comm = "SA-18 Igla comm",
-  Igla_manpad_INS = "Igla manpad INS",
-  SA18_IglaS_manpad = "SA-18 Igla-S manpad",
-  SA18_IglaS_comm = "SA-18 Igla-S comm",
-  Vulcan = "Vulcan",
-  Hawk_ln = "Hawk ln",
-  M48_Chaparral = "M48 Chaparral",
-  M6_Linebacker = "M6 Linebacker",
-  Patriot_ln = "Patriot ln",
-  M1097_Avenger = "M1097 Avenger",
-  Patriot_EPP = "Patriot EPP",
-  Patriot_cp = "Patriot cp",
-  Roland_ADS = "Roland ADS",
-  S300PS_54K6_cp = "S-300PS 54K6 cp",
-  Stinger_manpad_GRG = "Stinger manpad GRG",
-  Stinger_manpad_dsr = "Stinger manpad dsr",
-  Stinger_comm_dsr = "Stinger comm dsr",
-  Stinger_manpad = "Stinger manpad",
-  Stinger_comm = "Stinger comm",
-  ZSU234_Shilka = "ZSU-23-4 Shilka",
-  ZU23_Emplacement_Closed = "ZU-23 Emplacement Closed",
-  ZU23_Emplacement = "ZU-23 Emplacement",
-  ZU23_Closed_Insurgent = "ZU-23 Closed Insurgent",
-  Ural375_ZU23_Insurgent = "Ural-375 ZU-23 Insurgent",
-  ZU23_Insurgent = "ZU-23 Insurgent",
-  Ural375_ZU23 = "Ural-375 ZU-23"
-}
-unitTypes.vehicles.radar = {
-  _1L13_EWR = "1L13 EWR",
-  Kub_1S91_str = "Kub 1S91 str",
-  S300PS_40B6M_tr = "S-300PS 40B6M tr",
-  S300PS_40B6MD_sr = "S-300PS 40B6MD sr",
-  _55G6_EWR = "55G6 EWR",
-  S300PS_64H6E_sr = "S-300PS 64H6E sr",
-  SA11_Buk_SR_9S18M1 = "SA-11 Buk SR 9S18M1",
-  Dog_Ear_radar = "Dog Ear radar",
-  Hawk_tr = "Hawk tr",
-  Hawk_sr = "Hawk sr",
-  Patriot_str = "Patriot str",
-  Hawk_cwar = "Hawk cwar",
-  p19_s125_sr = "p-19 s-125 sr",
-  Roland_Radar = "Roland Radar",
-  snr_s125_tr = "snr s-125 tr"
-}
-unitTypes.vehicles.Structures = {
-  house1arm = "house1arm",
-  house2arm = "house2arm",
-  outpost_road = "outpost_road",
-  outpost = "outpost",
-  houseA_arm = "houseA_arm"
-}
-unitTypes.vehicles.Tanks = {
-  Challenger2 = "Challenger2",
-  Leclerc = "Leclerc",
-  Leopard1A3 = "Leopard1A3",
-  Leopard2 = "Leopard-2",
-  M60 = "M-60",
-  M1128_Stryker_MGS = "M1128 Stryker MGS",
-  M1_Abrams = "M-1 Abrams",
-  T55 = "T-55",
-  T72B = "T-72B",
-  T80UD = "T-80UD",
-  T90 = "T-90"
-}
-unitTypes.vehicles.unarmed = {
-  Ural4320_APA5D = "Ural-4320 APA-5D",
-  ATMZ5 = "ATMZ-5",
-  ATZ10 = "ATZ-10",
-  GAZ3307 = "GAZ-3307",
-  GAZ3308 = "GAZ-3308",
-  GAZ66 = "GAZ-66",
-  M978_HEMTT_Tanker = "M978 HEMTT Tanker",
-  HEMTT_TFFT = "HEMTT TFFT",
-  IKARUS_Bus = "IKARUS Bus",
-  KAMAZ_Truck = "KAMAZ Truck",
-  LAZ_Bus = "LAZ Bus",
-  Hummer = "Hummer",
-  M_818 = "M 818",
-  MAZ6303 = "MAZ-6303",
-  Predator_GCS = "Predator GCS",
-  Predator_TrojanSpirit = "Predator TrojanSpirit",
-  Suidae = "Suidae",
-  Tigr_233036 = "Tigr_233036",
-  Trolley_bus = "Trolley bus",
-  UAZ469 = "UAZ-469",
-  Ural_ATsP6 = "Ural ATsP-6",
-  Ural375_PBU = "Ural-375 PBU",
-  Ural375 = "Ural-375",
-  Ural432031 = "Ural-4320-31",
-  Ural4320T = "Ural-4320T",
-  VAZ_Car = "VAZ Car",
-  ZiL131_APA80 = "ZiL-131 APA-80",
-  SKP11 = "SKP-11",
-  ZIL131_KUNG = "ZIL-131 KUNG",
-  ZIL4331 = "ZIL-4331"
-}
