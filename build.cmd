96f4489d0d750153e5d2dc584171ecfabd3f3778
davidp57 <dpierron@gmail.com>
2018-08-27 11:54:15 +0200

    Changed the project so that the common scripts are in VEAF_mission_library.
    Changed the project so that the common scripts are in VEAF_mission_library.
    Updated the build scripts and documentation

--- build.cmd
+++ build.cmd
@@ -1,29 +1,39 @@
 @echo off
-set ZIP=temp.zip
-set MIZ=OT_Causacus_%date:~-4,4%%date:~-10,2%%date:~-7,2%.miz
-set MIZ_BUILD=test_%date:~-4,4%%date:~-10,2%%date:~-7,2%%time:~0,2%%time:~3,2%%time:~6,2%.miz
 
-echo %MIZ%
+IF [%VEAF_LIBRARY_FOLDER%] == [] GOTO DefineDefaultVEAF_LIBRARY_FOLDER
+goto DontDefineDefaultVEAF_LIBRARY_FOLDER
+:DefineDefaultVEAF_LIBRARY_FOLDER
+set VEAF_LIBRARY_FOLDER=..\VEAF_mission_library
+:DontDefineDefaultVEAF_LIBRARY_FOLDER
+echo VEAF_LIBRARY_FOLDER = %VEAF_LIBRARY_FOLDER%
+IF [%MISSION_FILE%] == [] GOTO DefineDefaultMISSION_FILE
+goto DontDefineDefaultMISSION_FILE
+:DefineDefaultMISSION_FILE
+set MISSION_FILE=.\build\OT_Causacus_%date:~-4,4%%date:~-10,2%%date:~-7,2%.miz
+:DontDefineDefaultMISSION_FILE
+echo MISSION_FILE = %MISSION_FILE%
 
-rem build new MIZ
-rd /s /q tempsrc
-mkdir tempsrc
-rd /s /q build
-mkdir build
-rd /s /q mission
-mkdir mission
-xcopy /y /e src tempsrc\
-copy community\mist_4_3_74.lua .\tempsrc\l10n\Default
-copy community\WeatherMark.lua .\tempsrc\l10n\Default
-copy scripts\veaf.lua .\tempsrc\l10n\Default
-copy scripts\veafMarkers.lua .\tempsrc\l10n\Default
-copy scripts\veafSpawn.lua .\tempsrc\l10n\Default
-copy scripts\veafCasMission.lua .\tempsrc\l10n\Default
-copy scripts\veafMove.lua .\tempsrc\l10n\Default
-copy scripts\veafGrass.lua .\tempsrc\l10n\Default
-pushd tempsrc
-7za a -r ..\build\%ZIP% -mem=AES256 *
-popd
-rd /s /q tempsrc
-copy build\%ZIP% mission\%MIZ%
-rd /s /q build
+rem -- prepare the folders
+rd /s /q .\build >nul 2>&1
+mkdir .\build >nul 2>&1
+mkdir .\build\tempsrc >nul 2>&1
+
+rem -- copy all the source mission files
+xcopy /y /e src .\build\tempsrc >nul 2>&1\
+
+rem -- copy all the scripts
+copy community\mist_4_3_74.lua .\build\tempsrc\l10n\Default >nul 2>&1
+copy community\WeatherMark.lua .\build\tempsrc\l10n\Default >nul 2>&1
+copy %VEAF_LIBRARY_FOLDER%\scripts\veaf.lua .\build\tempsrc\l10n\Default >nul 2>&1
+copy %VEAF_LIBRARY_FOLDER%\scripts\veafMarkers.lua .\build\tempsrc\l10n\Default >nul 2>&1
+copy %VEAF_LIBRARY_FOLDER%\scripts\veafSpawn.lua .\build\tempsrc\l10n\Default >nul 2>&1
+copy %VEAF_LIBRARY_FOLDER%\scripts\veafCasMission.lua .\build\tempsrc\l10n\Default >nul 2>&1
+copy %VEAF_LIBRARY_FOLDER%\scripts\veafMove.lua .\build\tempsrc\l10n\Default >nul 2>&1
+copy %VEAF_LIBRARY_FOLDER%\scripts\veafGrass.lua .\build\tempsrc\l10n\Default >nul 2>&1
+
+rem -- compile the mission
+7za a -r -tzip %MISSION_FILE% .\build\tempsrc\* -mem=AES256 * >nul 2>&1
+popd >nul 2>&1
+
+rem -- done !
+echo Built %MISSION_FILE%
