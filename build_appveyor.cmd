319daadf48313b46ebff3fcb92d4a6fcdfa5f8bf
David Pierron <dpierron@gmail.com>
2018-09-03 17:22:32 +0200

    Update and rename build.cmd to build_appveyor.cmd

--- build_appveyor.cmd
+++ build_appveyor.cmd
@@ -1,17 +1,6 @@
-@echo off
-
-IF [%VEAF_LIBRARY_FOLDER%] == [] GOTO DefineDefaultVEAF_LIBRARY_FOLDER
-goto DontDefineDefaultVEAF_LIBRARY_FOLDER
-:DefineDefaultVEAF_LIBRARY_FOLDER
+rem @echo off
 set VEAF_LIBRARY_FOLDER=..\VEAF_mission_library
-:DontDefineDefaultVEAF_LIBRARY_FOLDER
-echo VEAF_LIBRARY_FOLDER = %VEAF_LIBRARY_FOLDER%
-IF [%MISSION_FILE%] == [] GOTO DefineDefaultMISSION_FILE
-goto DontDefineDefaultMISSION_FILE
-:DefineDefaultMISSION_FILE
 set MISSION_FILE=.\build\OT_Causacus_%date:~-4,4%%date:~-10,2%%date:~-7,2%.miz
-:DontDefineDefaultMISSION_FILE
-echo MISSION_FILE = %MISSION_FILE%
 
 rem -- prepare the folders
 rd /s /q .\build 
