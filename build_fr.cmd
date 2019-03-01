2f548168c4922b73722d945553fb7295e42f9a22
David Pierron <dpierron@gmail.com>
2019-03-01 18:13:22 +0100

    Retour de l'openbeta et changements divers
    Saison : printemps
    Ajout du Mig19 dans la branche OpenBeta
    Renommage des vols AV8 dans la branche Release

--- build_fr.cmd
+++ build_fr.cmd
@@ -1,5 +1,4 @@
 @echo off
-set SEVENZIP=C:\tools\bin\7za.exe
 rem -- prepare the folders
 rd /s /q .\build  >nul 2>&1
 mkdir .\build  >nul 2>&1
@@ -7,4 +6,4 @@ mkdir .\build\tempsrc  >nul 2>&1
 
 set MISSION_FILE=.\build\OT_Causacus_%date:~-4,4%%date:~-7,2%%date:~-10,2%
 call build_one.cmd release
-rem call build_one.cmd openbeta
+call build_one.cmd openbeta
