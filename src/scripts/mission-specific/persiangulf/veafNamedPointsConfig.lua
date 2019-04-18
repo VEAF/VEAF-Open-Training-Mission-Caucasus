3caa9c58bc0a4d8694c08838b36ba183015f98d4
David Pierron <dpierron@gmail.com>
2019-04-18 22:07:56 +0200

    multiple changes
    bug CA : forgot to deactivate the RED F5 group
    feature CA : reset the briefing text
    bug PG : set client planes to AI on Khasab
    feature PG : redone the AirQuake planes
    feature PG : moved Arco to the N-E of Khasab
    feature PG : set the escort F15 to "use radar for attack only"
    feature PG : reset the briefing text

--- src/scripts/mission-specific/persiangulf/veafNamedPointsConfig.lua
+++ src/scripts/mission-specific/persiangulf/veafNamedPointsConfig.lua
@@ -21,10 +21,10 @@ veafNamedPoints.Points = {
     {name="AIRBASE Al Dhafra",point={x=-211168,y=0,z=-173476}},
     -- farps
     {name="FARP Berlin",point={x=-218745,y=0,z=-93998}},
-    {name="FARP Kish",point={x=62077,y=0,z=-188061}},
+    {name="FARP London",point={x=-66741,y=0,z=-17556}},
     -- points of interest
     {name="RANGE",point={x=-328289,y=0,z=631228}},
-    {name="WAR BlueBase",point={x=62638,y=0,z=-186423}}, 
-    {name="WAR RedBase",point={x=56709,y=0,z=-157838}},
-    {name="WAR Objective1",point={x=59435,y=0,z=-172369}},
+    {name="WAR BlueBase",point={x=-94461,y=0,z=-8323}}, 
+    {name="WAR RedBase",point={x=-64104,y=0,z=4106}},
+    {name="WAR Objective1",point={x=-76759,y=0,z=-6666}},
 }
