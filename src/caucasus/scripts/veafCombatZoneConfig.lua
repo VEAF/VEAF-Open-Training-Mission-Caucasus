8edfd6d93a8d2ae7f1ca38633f22438bad0b303e
David Pierron <dpierron@gmail.com>
2019-12-26 13:44:56 +0100

    New zones
    - Battle for Beslan (huge A2G zone)
    - Terek logistics parking (easy A2G zone)

--- src/caucasus/scripts/veafCombatZoneConfig.lua
+++ src/caucasus/scripts/veafCombatZoneConfig.lua
@@ -19,11 +19,31 @@ if veafCombatZone then
 	veafCombatZone.logInfo("Loading configuration")
 
 	veafCombatZone.AddZone(
-		Zone:new()
+		VeafCombatZone.new()
 			:setMissionEditorZoneName("combatZone_Psebay_Factory")
 			:setFriendlyName("Psebay chemical weapons factory")
 			:setBriefing("This factory manufactures chemical weapons for a terrorits group\n" ..
 						 "You must destroy both factory buildings, and the bunker where the scientists work\n" ..
 						 "The other enemy units are secondary targets\n")
 	)
+
+	veafCombatZone.AddZone(
+		VeafCombatZone.new()
+			:setMissionEditorZoneName("combatZone_BattleOfBeslan")
+			:setFriendlyName("Battle of Beslan")
+			:setBriefing("This zone is the place of a huge battle between red and blue armies.\n" ..
+						 "You must do what you can to help your side win\n" ..
+						 "Warning : there are air defenses lurking about, you should be cautious !")
+	)
+
+	veafCombatZone.AddZone(
+		VeafCombatZone.new()
+			:setMissionEditorZoneName("combatZone_EasyPickingsTerek")
+			:setFriendlyName("Terek logistics parking")
+			:setBriefing("The enemy has parked a lot of logistics at Terek\n" ..
+						 "You must destroy all the trucks to impend the advance of their army on Beslan\n" ..
+						 "The other enemy units are secondary targets\n"..
+						 "This is a more easy zone, with few air defenses. But beware that there is a chance of manpad in the area !")
+	)
+
 end
