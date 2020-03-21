41bdca819f804e5d56cd7d9c44500c2e6b32af90
David Pierron <dpierron@gmail.com>
2020-03-21 18:21:14 +0100

    Updated to the new freq plan
    also :
    - changed blue airports to neutral
    - corrected the frequencies for A10s
    - added A10s on Kutaisi
    - corrected the plane specific kneeboards
    - adapted the weather and start times, and changed the missions names

---
+++ src/weatherAndTime/0200-scattered.lua
@@ -0,0 +1,65 @@
+weatherAndTime = {
+    ["date"] = {
+        ["Day"] = 23,
+        ["Month"] = 5,
+        ["Year"] = 2011,
+    }, -- end of ["date"]
+
+    ["start_time"] = 7200,
+
+    ["weather"] = {
+        ["atmosphere_type"] = 0,
+        ["clouds"] = {
+            ["base"] = 3200,
+            ["density"] = 5,
+            ["iprecptns"] = 0,
+            ["thickness"] = 830,
+        }, -- end of ["clouds"]
+        ["cyclones"] = {
+            [1] = {
+                ["centerX"] = -902504.06819404,
+                ["centerZ"] = 1084702.1808043,
+                ["ellipticity"] = 0.92741635833369,
+                ["pressure_excess"] = -1237,
+                ["pressure_spread"] = 984553.65307475,
+                ["rotation"] = -0.14119397163551,
+            }, -- end of [1]
+        }, -- end of ["cyclones"]
+        ["dust_density"] = 0,
+        ["enable_dust"] = false,
+        ["enable_fog"] = true,
+        ["fog"] = {
+            ["thickness"] = 200,
+            ["visibility"] = 2800,
+        }, -- end of ["fog"]
+        ["groundTurbulence"] = 7.62,
+        ["name"] = "Spring, Thunderstorm",
+        ["name_cn"] = "春季 暴风雨",
+        ["name_de"] = "Frühling, Gewitter",
+        ["name_es"] = "Primavera. Tormenta",
+        ["name_fr"] = "Printemps, orages",
+        ["name_ru"] = "Весна. Гроза",
+        ["qnh"] = 753.872,
+        ["season"] = {
+            ["temperature"] = 18,
+        }, -- end of ["season"]
+        ["type_weather"] = 2,
+        ["visibility"] = {
+            ["distance"] = 80000,
+        }, -- end of ["visibility"]
+        ["wind"] = {
+            ["at2000"] = {
+                ["dir"] = 214,
+                ["speed"] = 7.731,
+            }, -- end of ["at2000"]
+            ["at8000"] = {
+                ["dir"] = 105,
+                ["speed"] = 12.885,
+            }, -- end of ["at8000"]
+            ["atGround"] = {
+                ["dir"] = 143,
+                ["speed"] = 6.1848,
+            }, -- end of ["atGround"]
+        }, -- end of ["wind"]
+    }, -- end of ["weather"]
+}
