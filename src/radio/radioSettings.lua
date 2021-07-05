bc0eb20c5cbc7bba5150948a510b986471e0275e
David Pierron <dpierron@gmail.com>
2021-07-05 20:41:43 +0200

    added blue MiG-21Bis, and set the radio presets

--- src/radio/radioSettings.lua
+++ src/radio/radioSettings.lua
@@ -874,6 +874,37 @@ radioSettings =
             }, -- end of [2]
         }, -- end of ["Radio"]
     },
+    ["blue MiG-21 Bis"] =
+    {
+        type = "MiG-21Bis",
+        coalition = "blue",
+        country = nil,
+        ["Radio"] = {
+            [1] = {
+                ["channels"] = {
+                    [1]  = radioPresets["##RADIO1_01##"],
+                    [2]  = radioPresets["##RADIO1_02##"],
+                    [3]  = radioPresets["##RADIO1_03##"],
+                    [4]  = radioPresets["##RADIO1_04##"],
+                    [5]  = radioPresets["##RADIO1_05##"],
+                    [6]  = radioPresets["##RADIO1_06##"],
+                    [7]  = radioPresets["##RADIO1_07##"],
+                    [8]  = radioPresets["##RADIO1_08##"],
+                    [9]  = radioPresets["##RADIO1_09##"],
+                    [10] = radioPresets["##RADIO1_10##"],
+                    [11] = radioPresets["##RADIO1_11##"],
+                    [12] = radioPresets["##RADIO1_12##"],
+                    [13] = radioPresets["##RADIO1_13##"],
+                    [14] = radioPresets["##RADIO1_14##"],
+                    [15] = radioPresets["##RADIO1_15##"],
+                    [16] = radioPresets["##RADIO1_16##"],
+                    [17] = radioPresets["##RADIO1_17##"],
+                    [18] = radioPresets["##RADIO1_18##"],
+                    [19] = radioPresets["##RADIO1_19##"],
+                    [20] = radioPresets["##RADIO1_20##"],                }, -- end of ["channels"]
+            }, -- end of [1]
+        }, -- end of ["Radio"]
+    },
     ["blue L-39C"] =
     {
         type = "L-39C",
