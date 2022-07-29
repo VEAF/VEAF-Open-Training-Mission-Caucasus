d0327bed4c1064d5a634874276d5eb0a9ced4ae2
RexAttaque <tristanmontagnier@gmail.com>
2022-07-29 19:57:13 +0200

    updated some interpreter commands to show units on MFD, Mirage F1CE radio presets

--- src/radio/radioSettings.lua
+++ src/radio/radioSettings.lua
@@ -575,6 +575,114 @@ radioSettings =
         }
     },
 
+    ["blue Mirage-F1CE"] =
+    {
+        type = "Mirage-F1CE",
+        coalition = "blue",
+        country = nil,
+        ["Radio"] =
+        {
+            [1] =
+            {
+                ["modulations"] =
+                {
+                    [1] =  0,
+                    [2] =  0,
+                    [3] =  0,
+                    [4] =  0,
+                    [5] =  0,
+                    [6] =  0,
+                    [7] =  0,
+                    [8] =  0,
+                    [9] =  0,
+                    [10] = 0,
+                    [11] = 0,
+                    [12] = 0,
+                    [13] = 0,
+                    [14] = 0,
+                    [15] = 0,
+                    [16] = 0,
+                    [17] = 0,
+                    [18] = 0,
+                    [19] = 0,
+                    [20] = 0,
+                }, -- end of ["modulations"]
+                ["channels"] =
+                {
+                    [1]  = radioPresets["##RADIO2_01##"],
+                    [2]  = radioPresets["##RADIO2_02##"],
+                    [3]  = radioPresets["##RADIO2_03##"],
+                    [4]  = radioPresets["##RADIO2_04##"],
+                    [5]  = radioPresets["##RADIO2_05##"],
+                    [6]  = radioPresets["##RADIO2_06##"],
+                    [7]  = radioPresets["##RADIO2_07##"],
+                    [8]  = radioPresets["##RADIO2_08##"],
+                    [9]  = radioPresets["##RADIO2_09##"],
+                    [10] = radioPresets["##RADIO2_10##"],
+                    [11] = radioPresets["##RADIO2_11##"],
+                    [12] = radioPresets["##RADIO2_12##"],
+                    [13] = radioPresets["##RADIO2_13##"],
+                    [14] = radioPresets["##RADIO2_14##"],
+                    [15] = radioPresets["##RADIO2_15##"],
+                    [16] = radioPresets["##RADIO2_16##"],
+                    [17] = radioPresets["##RADIO2_17##"],
+                    [18] = radioPresets["##RADIO2_18##"],
+                    [19] = radioPresets["##RADIO2_19##"],
+                    [20] = radioPresets["##RADIO2_20##"],
+                }, -- end of ["channels"]
+            }, -- end of [1]
+            [2] =
+            {
+                ["modulations"] =
+                {
+                    [1] =  0,
+                    [2] =  0,
+                    [3] =  0,
+                    [4] =  0,
+                    [5] =  0,
+                    [6] =  0,
+                    [7] =  0,
+                    [8] =  0,
+                    [9] =  0,
+                    [10] = 0,
+                    [11] = 0,
+                    [12] = 0,
+                    [13] = 0,
+                    [14] = 0,
+                    [15] = 0,
+                    [16] = 0,
+                    [17] = 0,
+                    [18] = 0,
+                    [19] = 0,
+                    [20] = 0,
+                }, -- end of ["modulations"]
+                ["channels"] =
+                {
+                    [1]  = radioPresets["##RADIO1_01##"],
+                    [2]  = radioPresets["##RADIO1_02##"],
+                    [3]  = radioPresets["##RADIO1_03##"],
+                    [4]  = radioPresets["##RADIO1_04##"],
+                    [5]  = radioPresets["##RADIO1_05##"],
+                    [6]  = radioPresets["##RADIO1_06##"],
+                    [7]  = radioPresets["##RADIO1_07##"],
+                    [8]  = radioPresets["##RADIO1_08##"],
+                    [9]  = radioPresets["##RADIO1_09##"],
+                    [10] = radioPresets["##RADIO1_10##"],
+                    [11] = radioPresets["##RADIO1_11##"],
+                    [12] = radioPresets["##RADIO1_12##"],
+                    [13] = radioPresets["##RADIO1_13##"],
+                    [14] = radioPresets["##RADIO1_14##"],
+                    [15] = radioPresets["##RADIO1_15##"],
+                    [16] = radioPresets["##RADIO1_16##"],
+                    [17] = radioPresets["##RADIO1_17##"],
+                    [18] = radioPresets["##RADIO1_18##"],
+                    [19] = radioPresets["##RADIO1_19##"],
+                    [20] = radioPresets["##RADIO1_20##"],
+                }, -- end of ["channels"]
+            }, -- end of [2]
+        }
+    },
+
     ["blue Harrier"] =
     {
         type = "AV8BNA",
