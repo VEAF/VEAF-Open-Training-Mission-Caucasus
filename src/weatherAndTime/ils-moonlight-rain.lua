84b5077c6990092b0577882d042e19bb1394aef4
David Pierron <dpierron@gmail.com>
2023-03-15 21:25:49 +0100

    new ils-moonlight version

---
+++ src/weatherAndTime/ils-moonlight-rain.lua
@@ -0,0 +1,57 @@
+    ["weather"] = 
+    {
+        ["atmosphere_type"] = 0,
+        ["groundTurbulence"] = 0,
+        ["enable_fog"] = true,
+        ["halo"] = 
+        {
+            ["preset"] = "off",
+        }, -- end of ["halo"]
+        ["qnh"] = 771.652,
+        ["season"] = 
+        {
+            ["temperature"] = 8.536,
+        }, -- end of ["season"]
+        ["type_weather"] = 2,
+        ["modifiedTime"] = true,
+        ["cyclones"] = 
+        {
+        }, -- end of ["cyclones"]
+        ["enable_dust"] = false,
+        ["dust_density"] = 0,
+        ["fog"] = 
+        {
+            ["thickness"] = 990,
+            ["visibility"] = 640,
+        }, -- end of ["fog"]
+        ["wind"] = 
+        {
+            ["at8000"] = 
+            {
+                ["speed"] = 7.7516764008671,
+                ["dir"] = 142,
+            }, -- end of ["at8000"]
+            ["at2000"] = 
+            {
+                ["speed"] = 4.6489748624303,
+                ["dir"] = 77,
+            }, -- end of ["at2000"]
+            ["atGround"] = 
+            {
+                ["speed"] = 3,
+                ["dir"] = 80,
+            }, -- end of ["atGround"]
+        }, -- end of ["wind"]
+        ["visibility"] = 
+        {
+            ["distance"] = 80000,
+        }, -- end of ["visibility"]
+        ["clouds"] = 
+        {
+            ["density"] = 0,
+            ["thickness"] = 201,
+            ["preset"] = "RainyPreset2",
+            ["base"] = 2500,
+            ["iprecptns"] = 0,
+        }, -- end of ["clouds"]
+    }, -- end of ["weather"]
