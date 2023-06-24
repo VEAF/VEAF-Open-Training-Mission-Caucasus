25f57954769c7c45b64764b226ed721c2187bfbb
David Pierron <dpierron@gmail.com>
2023-06-24 17:04:40 +0200

    updated weather to match server

--- src/weatherAndTime/broken-1.lua
+++ src/weatherAndTime/broken-1.lua
@@ -1,53 +1,43 @@
-    ["weather"] = 
+["weather"] = {
+	["atmosphere_type"] = 0,
+    ["clouds"] = 
     {
-        ["wind"] = 
-        {
-            ["at8000"] = 
-            {
-                ["speed"] = 10,
-                ["dir"] = 278,
-            }, -- end of ["at8000"]
-            ["at2000"] = 
-            {
-                ["speed"] = 7,
-                ["dir"] = 63,
-            }, -- end of ["at2000"]
-            ["atGround"] = 
-            {
-                ["speed"] = 5,
-                ["dir"] = 248,
-            }, -- end of ["atGround"]
-        }, -- end of ["wind"]
-        ["enable_fog"] = false,
-        ["qnh"] = 740,
-        ["cyclones"] = 
-        {
-        }, -- end of ["cyclones"]
-        ["dust_density"] = 0,
-        ["enable_dust"] = false,
-        ["clouds"] = 
-        {
-            ["density"] = 8,
-            ["thickness"] = 300,
-            ["preset"] = "Preset10",
-            ["base"] = 2500,
-            ["iprecptns"] = 1,
-        }, -- end of ["clouds"]
-        ["atmosphere_type"] = 0,
-        ["groundTurbulence"] = 4,
-        ["type_weather"] = 0,
-        ["modifiedTime"] = true,
-        ["fog"] = 
-        {
-            ["thickness"] = 0,
-            ["visibility"] = 0,
-        }, -- end of ["fog"]
-        ["season"] = 
-        {
-            ["temperature"] = 23.200000762939,
-        }, -- end of ["season"]
-        ["visibility"] = 
-        {
-            ["distance"] = 80000,
-        }, -- end of ["visibility"]
-    }, -- end of ["weather"]
+        ["thickness"] = 200,
+        ["density"] = 0,
+        ["preset"] = "Preset13",
+        ["base"] = 3400,
+        ["iprecptns"] = 0,
+    }, -- end of ["clouds"]
+    ["cyclones"] = {
+	}, -- end of ["cyclones"]
+	["dust_density"] = 0,
+	["enable_dust"] = false,
+	["enable_fog"] = false,
+	["fog"] = {
+			["thickness"] = 0,
+			["visibility"] = 0,
+	}, -- end of ["fog"]
+	["groundTurbulence"] = 26.656422237728,
+	["qnh"] = 758.444,
+	["season"] = {
+			["temperature"] = 23.200000762939,
+	}, -- end of ["season"]
+	["type_weather"] = 2,
+	["visibility"] = {
+			["distance"] = 1593,
+	}, -- end of ["visibility"]
+	["wind"] = {
+			["at2000"] = {
+					["dir"] = 148,
+					["speed"] = 10.604474819794,
+			}, -- end of ["at2000"]
+			["at8000"] = {
+					["dir"] = 160,
+					["speed"] = 12.07985101455,
+			}, -- end of ["at8000"]
+			["atGround"] = {
+					["dir"] = 150,
+					["speed"] = 4.5,
+			}, -- end of ["atGround"]
+	}, -- end of ["wind"]
+}, -- end of ["weather"]
